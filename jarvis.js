cc8b669a54116483bc53cb3e53b937fa0c7d3ccd
krustnic <avmityakov@gmail.com>
2014-05-12 18:28:42 +0000

    add unit-tests basic

--- jarvis.js
+++ jarvis.js
@@ -9,6 +9,17 @@
 var Jarvis = new (function() {
     var self = this;
     
+    this.SCREENSHOT_PREFIX = "screen";
+    this.SCREENSHOT_EXT    = "png";
+    this._screenShotCount  = 0;
+        
+    this.getNewScreenshotName  = function() {
+        var newName = self.SCREENSHOT_PREFIX + "-" + self._screenShotCount + "." + self.SCREENSHOT_EXT;
+        self._screenShotCount += 1;
+        
+        return newName;
+    }
+    
     this.wrap = function( target, wrapFunction ) {
         var f = target;
         return function() {
@@ -21,13 +32,40 @@ var Jarvis = new (function() {
     }
 })();
 
+// For unit tests purpose
+if ( typeof exports != "undefined" ) exports.Jarvis = Jarvis;
+var casper = casper || {};
+//
+
 /**
+ * Original signature: capture(String targetFilepath, [Object clipRect, Object imgOptions])
  * Rewrite "capture" for making screenshots to the proper server location
  **/
 
 casper.capture = Jarvis.wrap( casper.capture, function( f, arguments ) {
-    f.call( casper, "screen-3.png" );
-    // var userFilePath = arguments[0] 
+    
+    // Only fileName
+    if ( arguments.length == 1 ) {
+        f.call( casper, Jarvis.getNewScreenshotName() );            
+        return;
+    }
+    
+    // Filename and clipRect
+    if ( arguments.length == 2 ) {
+        f.call( casper, Jarvis.getNewScreenshotName(), arguments[1] );            
+        return;
+    }
+    
+    // Filename, clipRect and imgOptions
+    // Ignoring third imgOptions - always create PNG image
+    if ( arguments.length == 3 ) {
+        f.call( casper, Jarvis.getNewScreenshotName(), arguments[1] );            
+        return;
+    }
+    
+    
+    f.call( casper, Jarvis.getNewScreenshotName() );    
+    
 });
 
 /**
---
+++ run-tests.sh
@@ -0,0 +1,1 @@
+nodeunit unit-tests/tests.js
